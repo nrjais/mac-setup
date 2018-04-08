8c158a1261f79f30dd92fa61211ce3ffd5e8a565
Neeraj Jaiswal <neerajj85@gmail.com>
2018-04-08 09:04:22 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,43 @@
+#!/usr/bin/env bash
+
+# Ask for the administrator password upfront.
+sudo -v
+
+# Keep-alive: update existing `sudo` time stamp until the script has finished.
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
+#Ask for name to use in oh my zsh
+echo "Enter name to display in prompt"
 read name
 
-echo "Installing homebrew";
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+# Check for Homebrew,
+# Install if we don't have it
+if test ! $(which brew); then
+  echo "Installing homebrew..."
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+# Make sure we’re using the latest Homebrew.
+brew update
+
+# Upgrade any already-installed formulae.
+brew upgrade --all
 
 export HOMEBREW_NO_AUTO_UPDATE=1
+
+brew install coreutils
+sudo ln -s /usr/local/bin/gsha256sum /usr/local/bin/sha256sum
+
+# Install some other useful utilities like `sponge`.
+brew install moreutils
+# Install GNU `find`, `locate`, `updatedb`, and `xargs`, `g`-prefixed.
+brew install findutils
+# Install GNU `sed`, overwriting the built-in `sed`.
+brew install gnu-sed --with-default-names
+
+brew install ack
+brew install git
+
 
 echo $name | sh -c "$(curl -fsSL https://raw.githubusercontent.com/nrjais/oh-my-zsh/develop/tools/install.sh)"
 
@@ -47,13 +81,15 @@ brew cask install visual-studio-code
 echo "Installing adobe reader";
 brew cask install adobe-acrobat-reader
 
-
 echo "Installing java";
 brew cask install java
-
 
 echo "Installing mocha";
 npm i -g mocha
 
 echo "Installing nodemon";
 npm i -g nodemon
+
+
+#Cleanup
+brew cleanup
